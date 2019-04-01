9bf6d36df00d10adb34c4ee28e4f56154de15470
pengyun <koaidw@163.com>
2019-04-02 02:23:18 +0800

    'init'

---
+++ src/basic/sort/insertion.rs
@@ -0,0 +1,28 @@
+use std::cmp::PartialOrd;
+
+pub fn insertion_sort<T>(list: &mut [T])
+    where T: PartialOrd + Copy
+{
+    for i in 1..list.len() {
+        let mut j = i;
+        let tmp = list[j - 1];
+        while j > 0 && list[j - 1] > tmp {
+            list[j] = list[j - 1];
+            j -= 1;
+        }
+        list[j] = tmp;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let mut arr = vec![2, 1, 3, 5, 4];
+        insertion_sort(&mut arr);
+        println!("{:?}", arr);
+        assert_eq!(vec![1, 2, 3, 4, 5], arr);
+    }
+}
